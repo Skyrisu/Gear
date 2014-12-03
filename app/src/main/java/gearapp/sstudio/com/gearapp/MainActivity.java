1d3d63a7ed71f9f013b82d34e92e28d1089b407c
Marvin <marvinstarke@gmail.com>
2014-12-03 15:07:51 +0100

    Bug Fix <Api19

--- app/src/main/java/gearapp/sstudio/com/gearapp/MainActivity.java
+++ app/src/main/java/gearapp/sstudio/com/gearapp/MainActivity.java
@@ -34,16 +34,17 @@ public class MainActivity extends Activity {
         Animation gearanim = AnimationUtils.loadAnimation(MainActivity.this, R.anim.rotateloop);
         final ImageView gear = (ImageView) findViewById(R.id.NonFunctionGear);
         gear.startAnimation(gearanim);
-
-        getWindow().getDecorView()
-                .setSystemUiVisibility(
-                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                                | View.SYSTEM_UI_FLAG_FULLSCREEN
-                                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                                | View.INVISIBLE);
+        if(android.os.Build.VERSION.SDK_INT >= 19) {
+            getWindow().getDecorView()
+                    .setSystemUiVisibility(
+                            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                                    | View.INVISIBLE);
+        }
     }
 
 
